6856f209c705e01f8b02c298513b0c89ec07b3de
Emir Basic <ebasic@gmail.com>
2020-07-11 23:06:53 +0200

    prj-start

---
+++ project/RecipeBook/src/app/prj-start/prj-start.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RunComponent } from './run/run.component';
+
+
+@NgModule({
+  declarations: [
+    RunComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RunComponent
+  ]
+})
+export class PrjStartModule {
+}
