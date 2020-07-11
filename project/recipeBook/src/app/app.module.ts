cda8002e399e34ea08ff3c06e4871a6cde8f898b
Emir Basic <ebasic@gmail.com>
2020-07-11 23:40:42 +0200

    prj-start module

--- project/recipeBook/src/app/app.module.ts
+++ project/recipeBook/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PrjStartModule as RunModule } from './prj-start/prj-start.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RunModule
   ],
   providers: [],
   bootstrap: [AppComponent]
