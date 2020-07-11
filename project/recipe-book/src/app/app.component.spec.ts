98b09e4df51b09c007313ec8e7d18e8fa2cce88c
Emir Basic <ebasic@gmail.com>
2020-07-11 23:50:23 +0200

    rename recipeBook to recipe-book

--- project/recipe-book/src/app/app.component.spec.ts
+++ project/recipe-book/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipeBook'`, () => {
+  it(`should have as title 'recipe-book'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipeBook');
+    expect(app.title).toEqual('recipe-book');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('recipeBook app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('recipe-book app is running!');
   });
 });
