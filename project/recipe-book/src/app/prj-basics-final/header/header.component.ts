844a32abf5ccd4e3c20d55707036bd5241c3aaf1
Emir Basic <ebasic@gmail.com>
2020-07-12 23:35:21 +0200

    #50. Alternative Non-Collapsable Navigation Bar

--- project/recipe-book/src/app/prj-basics-final/header/header.component.ts
+++ project/recipe-book/src/app/prj-basics-final/header/header.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  collapsed = true;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
