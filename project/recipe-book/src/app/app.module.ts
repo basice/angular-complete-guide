136168d42e542132406f3b52967a14508a224703
Emir Basic <ebasic@gmail.com>
2020-07-13 15:01:23 +0200

    Section 6: Course Project - Components & Databinding

--- project/recipe-book/src/app/app.module.ts
+++ project/recipe-book/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 // import { PrjStartModule as RunModule } from './prj-start/prj-start.module';
-import { PrjBasicsFinalModule as RunModule } from './prj-basics-final/prj-basics-final.module';
+import { PrjCmpDatabindningFinalModule as RunModule } from './prj-cmp-databindning-final/prj-cmp-databindning-final.module';
 
 @NgModule({
   declarations: [
