699bfd2bf16d10db3eb353fbd80dbf3bcd372e17
Emir Basic <ebasic@gmail.com>
2020-07-13 22:40:36 +0200

    Section 8: Course Project - Directives

--- project/recipe-book/src/app/app.module.ts
+++ project/recipe-book/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { PrjStartModule as RunModule } from './prj-start/prj-start.module';
-import { PrjCmpDatabindningFinalModule as RunModule } from './prj-cmp-databindning-final/prj-cmp-databindning-final.module';
+// import { PrjCmpDatabindningFinalModule as RunModule } from './prj-cmp-databindning-final/prj-cmp-databindning-final.module';
+import { PrjDirectivesFinalModule as RunModule } from './prj-directives-final/prj-directives-final.module';
 
 @NgModule({
   declarations: [
---
+++ project/recipe-book/src/app/prj-directives-final/prj-directives-final.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RunComponent } from './run/run.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    RunComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  exports: [RunComponent]
+})
+export class PrjDirectivesFinalModule {
+}
