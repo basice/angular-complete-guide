2539f23bda32d6066e717d311ef1f35e72778424
Emir Basic <ebasic@gmail.com>
2020-07-13 14:14:36 +0200

    #55. Working on the ShoppingListComponent

--- project/recipe-book/src/app/prj-basics-final/shopping-list/shopping-list.component.ts
+++ project/recipe-book/src/app/prj-basics-final/shopping-list/shopping-list.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
