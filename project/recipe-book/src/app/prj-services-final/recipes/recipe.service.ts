180624189921fe39924913be4e6f6beb3c632dd4
Emir Basic <ebasic@gmail.com>
2020-07-14 10:36:50 +0200

    #117. Managing Recipes in a Recipe Service

--- project/recipe-book/src/app/prj-services-final/recipes/recipe.service.ts
+++ project/recipe-book/src/app/prj-services-final/recipes/recipe.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
-  constructor() {
+  getRecipes() {
+    return this.recipes.slice();
   }
 }
