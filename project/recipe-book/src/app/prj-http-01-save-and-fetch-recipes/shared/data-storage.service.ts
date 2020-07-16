3d7d9733bfc83753be51accb583e8649342afa72
Emir Basic <ebasic@gmail.com>
2020-07-16 20:50:52 +0200

    #281. Fetching Recipes

--- project/recipe-book/src/app/prj-http-01-save-and-fetch-recipes/shared/data-storage.service.ts
+++ project/recipe-book/src/app/prj-http-01-save-and-fetch-recipes/shared/data-storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,6 @@ export class DataStorageService {
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
-
     this.http
       .put('https://ng-course-recipe-book-85a9e.firebaseio.com/recipes.json',
         recipes)
@@ -18,6 +18,12 @@ export class DataStorageService {
         response => {
           console.log(response);
         });
+  }
 
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-course-recipe-book-85a9e.firebaseio.com/recipes.json')
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      });
   }
 }
