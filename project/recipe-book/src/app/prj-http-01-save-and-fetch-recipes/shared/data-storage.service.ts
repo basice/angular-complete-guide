50cab048572c6a3be1aa4d6bceacd5e23ef2eb53
Emir Basic <ebasic@gmail.com>
2020-07-16 21:18:53 +0200

    #282. Transforming Response Data

--- project/recipe-book/src/app/prj-http-01-save-and-fetch-recipes/shared/data-storage.service.ts
+++ project/recipe-book/src/app/prj-http-01-save-and-fetch-recipes/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,16 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://ng-course-recipe-book-85a9e.firebaseio.com/recipes.json')
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
+      )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       });
