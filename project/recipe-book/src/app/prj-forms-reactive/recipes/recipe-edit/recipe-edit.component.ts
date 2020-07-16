31514806bf7a5a729594c436811077ee1a46d804
Emir Basic <ebasic@gmail.com>
2020-07-16 18:59:16 +0200

    #233. Adding a Delete and Clear (Cancel) Functionality

--- project/recipe-book/src/app/prj-forms-reactive/recipes/recipe-edit/recipe-edit.component.ts
+++ project/recipe-book/src/app/prj-forms-reactive/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
@@ -15,7 +15,8 @@ export class RecipeEditComponent implements OnInit {
   recipeForm: FormGroup;
 
   constructor(private route: ActivatedRoute,
-              private recipeService: RecipeService) { }
+              private recipeService: RecipeService,
+              private router: Router) { }
 
   get controls() {
     return (this.recipeForm.get('ingredients') as FormArray).controls;
@@ -44,6 +45,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient() {
@@ -51,6 +53,11 @@ export class RecipeEditComponent implements OnInit {
       name: new FormControl(null, Validators.required),
       amount: new FormControl(null, Validators.pattern(/^[1-9]+[0-9]*$/))
     }));
+  }
+
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   private initForm() {
