c250770e985887c44a7f2ba26615a744ca6abd38
Emir Basic <ebasic@gmail.com>
2020-07-16 14:00:17 +0200

    #220. Loading the Shopping List Items into the Form

--- project/recipe-book/src/app/prj-forms-td/shopping-list/shopping-edit/shopping-edit.component.ts
+++ project/recipe-book/src/app/prj-forms-td/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
@@ -11,18 +11,24 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-
+  @ViewChild('f') slForm: NgForm;
   subscription: Subscription;
   private editMode = false;
   private editedItemIndex: number;
+  private editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.subscription = this.slService.startedEditing
       .subscribe((index: number) => {
-        this.editMode = true;
         this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
       });
   }
 
